4355de2eda0a71498d76d2f31d3567051729e99f
jsakamoto <junichi.jp@gmail.com>
2020-06-07 14:11:15 +0900

    Reproduce problem
    - add HTTP request to the Web API that's allowed anonymous access, but it cause AccessTokenNotAvailableException.

---
+++ Server/Controllers/RecentlyUpdatesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecentlyUpdatesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            yield return "Reproduce the problem.";
+            yield return "Initila commit.";
+        }
+    }
+}
